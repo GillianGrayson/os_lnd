0f6358bfa3242aa39214e060d9cc769bb54317fd
Archie Hewitt <hewitt.archie@yandex.ru>
2019-11-15 19:28:23 +0300

    added filesystem submodule

--- source/cpp/os_lnd/os_lnd/lindbladian_odeint_rk4_run_strategy.h
+++ source/cpp/os_lnd/os_lnd/lindbladian_odeint_rk4_run_strategy.h
@@ -4,7 +4,7 @@
 #include <boost/numeric/odeint.hpp>
 #include "odeint_init.h"
 #include "integrate_processor.h"
-#include <experimental/filesystem>
+#include "third_party/filesystem/include/ghc/filesystem.hpp"
 
 struct LindbladianODEIntRK4RunStrategy : RunStrategy
 {
@@ -20,7 +20,7 @@ struct LindbladianODEIntRK4RunStrategy : RunStrategy
 		auto fn_curr_dump = continue_path + "curr_dump" + model.suffix;
 		auto fn_rho_mtx = continue_path + "rho_mtx" + model.suffix;
 		
-		if (is_continue && std::experimental::filesystem::exists(fn_curr_dump) && std::experimental::filesystem::exists(fn_rho_mtx))
+		if (is_continue && ghc::filesystem::exists(fn_curr_dump) && ghc::filesystem::exists(fn_rho_mtx))
 		{
 			auto fn = continue_path + "curr_dump" + model.suffix;
 			std::vector<double> last_dump_info;
