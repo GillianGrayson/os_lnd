32da0d4c9e377a3254fd5739ff303105eca0b122
Mikhail Ivanchenko <ivanchen@newton.mpipks-dresden.mpg.de>
2020-02-09 22:22:59 +0100

    It compiles for now

--- source/cpp/os_lnd/os_lnd/smallest_eigen_vector_run_strategy.h
+++ source/cpp/os_lnd/os_lnd/smallest_eigen_vector_run_strategy.h
@@ -15,58 +15,58 @@ struct SmallestEigenVectorRunStrategy : RunStrategy
         EPSType        type;
         PetscReal      error, tol, re, im;
         PetscScalar    value, kr, ki;
+		PetscScalar    *sm_vector;
         Vec            xr, xi;
         PetscInt       n, i, Istart, Iend, nev, maxit, its, nconv;
-        PetscErrorCode ierr;
 
 		n = model.sys_size;
 
 		/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 		   Compute the operator matrix that defines the eigensystem, Ax=kx
 		   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-        ierr = MatCreate(PETSC_COMM_WORLD, &A); CHKERRQ(ierr);
-        ierr = MatSetSizes(A, PETSC_DECIDE, PETSC_DECIDE, n, n); CHKERRQ(ierr);
-        ierr = MatSetFromOptions(A); CHKERRQ(ierr);
-        ierr = MatSetUp(A); CHKERRQ(ierr);
+        MatCreate(PETSC_COMM_WORLD, &A);
+        MatSetSizes(A, PETSC_DECIDE, PETSC_DECIDE, n, n);
+        MatSetFromOptions(A);
+        MatSetUp(A);
 		for (int k = 0; k < model.lindbladian.outerSize(); ++k)
 		{
 			for (typename sp_mtx::InnerIterator it(model.lindbladian, k); it; ++it)
 			{
 				value = it.value().real() + it.value().imag() * PETSC_i;			
-				ierr = MatSetValue(A, it.row(), it.col(), value, INSERT_VALUES); CHKERRQ(ierr);
+				MatSetValue(A, it.row(), it.col(), value, INSERT_VALUES);
 			}
 		}
-		ierr = MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
-		ierr = MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+		MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);
+		MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);
 
-		ierr = MatCreateVecs(A, NULL, &xr); CHKERRQ(ierr);
-		ierr = MatCreateVecs(A, NULL, &xi); CHKERRQ(ierr);
+		MatCreateVecs(A, NULL, &xr);
+		MatCreateVecs(A, NULL, &xi);
 
 		/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 					  Create the eigensolver and set various options
 		   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-		ierr = EPSCreate(PETSC_COMM_WORLD, &eps); CHKERRQ(ierr);
-		ierr = EPSSetOperators(eps, A, NULL); CHKERRQ(ierr);
-		ierr = EPSSetProblemType(eps, EPS_NHEP); CHKERRQ(ierr);
-		ierr = EPSSetWhichEigenpairs(eps, EPS_SMALLEST_MAGNITUDE); CHKERRQ(ierr);
-		ierr = EPSSetFromOptions(eps); CHKERRQ(ierr);
+		EPSCreate(PETSC_COMM_WORLD, &eps);
+		EPSSetOperators(eps, A, NULL);
+		EPSSetProblemType(eps, EPS_NHEP);
+		EPSSetWhichEigenpairs(eps, EPS_SMALLEST_MAGNITUDE);
+		EPSSetFromOptions(eps);
 
 
 		/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 							Solve the eigensystem
 		   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-		ierr = EPSSolve(eps); CHKERRQ(ierr);
+		EPSSolve(eps);
 		/*
 		   Optional: Get some information from the solver and display it
 		*/
-		ierr = EPSGetIterationNumber(eps, &its); CHKERRQ(ierr);
-		ierr = PetscPrintf(PETSC_COMM_WORLD, " Number of iterations of the method: %D\n", its); CHKERRQ(ierr);
-		ierr = EPSGetType(eps, &type); CHKERRQ(ierr);
-		ierr = PetscPrintf(PETSC_COMM_WORLD, " Solution method: %s\n\n", type); CHKERRQ(ierr);
-		ierr = EPSGetDimensions(eps, &nev, NULL, NULL); CHKERRQ(ierr);
-		ierr = PetscPrintf(PETSC_COMM_WORLD, " Number of requested eigenvalues: %D\n", nev); CHKERRQ(ierr);
-		ierr = EPSGetTolerances(eps, &tol, &maxit); CHKERRQ(ierr);
-		ierr = PetscPrintf(PETSC_COMM_WORLD, " Stopping condition: tol=%.4g, maxit=%D\n", (double)tol, maxit); CHKERRQ(ierr);
+		EPSGetIterationNumber(eps, &its);
+		PetscPrintf(PETSC_COMM_WORLD, " Number of iterations of the method: %D\n", its);
+		EPSGetType(eps, &type);
+		PetscPrintf(PETSC_COMM_WORLD, " Solution method: %s\n\n", type);
+		EPSGetDimensions(eps, &nev, NULL, NULL);
+		PetscPrintf(PETSC_COMM_WORLD, " Number of requested eigenvalues: %D\n", nev);
+		EPSGetTolerances(eps, &tol, &maxit);
+		PetscPrintf(PETSC_COMM_WORLD, " Stopping condition: tol=%.4g, maxit=%D\n", (double)tol, maxit);
 
 		/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 							Display solution and clean up
@@ -74,37 +74,40 @@ struct SmallestEigenVectorRunStrategy : RunStrategy
 		/*
 				Get number of converged approximate eigenpairs
 		*/
-		ierr = EPSGetConverged(eps, &nconv); CHKERRQ(ierr);
-		ierr = PetscPrintf(PETSC_COMM_WORLD, " Number of converged eigenpairs: %D\n\n", nconv); CHKERRQ(ierr);
+		EPSGetConverged(eps, &nconv);
+		PetscPrintf(PETSC_COMM_WORLD, " Number of converged eigenpairs: %D\n\n", nconv);
 
-		if (nconv > 0) 
-		{
-			/*
-			   Display eigenvalue and relative error
-			*/
+		/*
+		   Display eigenvalue and relative error
+		*/
 
-			ierr = EPSGetEigenpair(eps, 0, &kr, &ki, xr, xi); CHKERRQ(ierr);
-			/*
-			   Compute the relative error associated to eigenpair
-			*/
-			ierr = EPSComputeError(eps, 0, EPS_ERROR_RELATIVE, &error); CHKERRQ(ierr);
+		EPSGetEigenpair(eps, 0, &kr, &ki, xr, xi);
+		/*
+		   Compute the relative error associated to eigenpair
+		*/
+		EPSComputeError(eps, 0, EPS_ERROR_RELATIVE, &error);
 
-			re = PetscRealPart(kr);
-			im = PetscImaginaryPart(kr);
+		re = PetscRealPart(kr);
+		im = PetscImaginaryPart(kr);
 
-			if (im != 0.0) {
-				ierr = PetscPrintf(PETSC_COMM_WORLD, " %9f%+9fi %12g\n", (double)re, (double)im, (double)error); CHKERRQ(ierr);
-			}
-			else {
-				ierr = PetscPrintf(PETSC_COMM_WORLD, "   %12f       %12g\n", (double)re, (double)error); CHKERRQ(ierr);
-			}
+		if (im != 0.0) {
+			PetscPrintf(PETSC_COMM_WORLD, " %9f%+9fi %12g\n", (double)re, (double)im, (double)error);
 		}
+		else {
+			PetscPrintf(PETSC_COMM_WORLD, "   %12f       %12g\n", (double)re, (double)error);
+		}
+		
+		VecGetArray(xr, &sm_vector);
 
 		Eigen::VectorXcd rho_vec(model.sys_size * model.sys_size);
 		for (int st_id = 0; st_id < model.sys_size * model.sys_size; st_id++)
 		{
-			rho_vec[st_id] = std::complex<double>(PetscRealPart(xr[st_id]), PetscImaginaryPart(xr[st_id]));
+			double real_part = double(PetscRealPart(sm_vector[st_id]));
+			double imag_part = double(PetscImaginaryPart(sm_vector[st_id]));
+			rho_vec[st_id] = std::complex<double>(real_part, imag_part);
 		}
+		
+		VecRestoreArray(xr, &sm_vector);
 
 		model.rho = Eigen::Map<Eigen::MatrixXcd>(rho_vec.data(), model.sys_size, model.sys_size);
 		auto trace_rho = model.rho.trace();
@@ -119,9 +122,9 @@ struct SmallestEigenVectorRunStrategy : RunStrategy
 		/*
 		   Free work space
 		*/
-		ierr = EPSDestroy(&eps); CHKERRQ(ierr);
-		ierr = MatDestroy(&A); CHKERRQ(ierr);
-		ierr = VecDestroy(&xr); CHKERRQ(ierr);
-		ierr = VecDestroy(&xi); CHKERRQ(ierr);
+		EPSDestroy(&eps);
+		MatDestroy(&A);
+		VecDestroy(&xr);
+		VecDestroy(&xi);
 	}
 };
