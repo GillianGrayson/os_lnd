3ed5119c973e2e5978828b389957e3c7e5d745a0
Unknown <hewitt.archie@yandex.ru>
2020-02-21 15:19:54 +0300

    set all to default

--- source/cpp/os_lnd/os_lnd/all_evals_run_strategy.h
+++ source/cpp/os_lnd/os_lnd/all_evals_run_strategy.h
@@ -81,9 +81,9 @@ struct AllEvalsRunStrategy : RunStrategy
 		EPSCreate(PETSC_COMM_WORLD, &eps);
 		EPSSetOperators(eps, A, NULL);
 		EPSSetProblemType(eps, EPS_NHEP);
-		EPSSetWhichEigenpairs(eps, EPS_SMALLEST_MAGNITUDE);
-		EPSSetType(eps, EPSKRYLOVSCHUR);
-		EPSSetTolerances(eps, tolerance, max_num_iterations);
+		//EPSSetWhichEigenpairs(eps, EPS_SMALLEST_MAGNITUDE);
+		//EPSSetType(eps, EPSKRYLOVSCHUR);
+		//EPSSetTolerances(eps, tolerance, max_num_iterations);
 		EPSSetDimensions(eps, n, PETSC_DEFAULT, PETSC_DEFAULT);
 		EPSSetFromOptions(eps);
 
