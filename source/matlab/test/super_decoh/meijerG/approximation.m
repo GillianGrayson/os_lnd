2b4ae76cd9e56ff1f50a73f732936c834ba80d85
Archie Hewitt <hewitt.archie@yandex.ru>
2020-10-31 22:28:03 +0300

    paper figs

--- source/matlab/test/super_decoh/meijerG/approximation.m
+++ source/matlab/test/super_decoh/meijerG/approximation.m
@@ -1,6 +1,14 @@
 clear all;
-x = linspace(-2, 2, 500);
-z = x.^2 / 4;
+x = linspace(-2, 2, 500)';
+x2 = x.^2;
+tmp = 4.0 ./ x2;
+arg = 1. - 4./(x.^2);
+
+second = ellipticE(arg);
+first = ellipke(arg);
+
+
+y = 2 * abs(x) * ((4.+x.^2) * second - 8. * first)/(3 * pi.^2);
 
 y1 = meijerG([1, 2], [], [], [-0.5, 0.5],  z)';
 aaa = meijerG(1, 2, -0.5, 0.5,  z)';
