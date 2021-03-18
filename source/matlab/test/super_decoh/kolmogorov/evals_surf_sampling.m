c96c06d125a7738b70a85fba32f3213783771e8f
Archie Hewitt <hewitt.archie@yandex.ru>
2021-03-18 20:48:24 +0300

    plots again ...

--- source/matlab/test/super_decoh/kolmogorov/evals_surf_sampling.m
+++ source/matlab/test/super_decoh/kolmogorov/evals_surf_sampling.m
@@ -1,32 +1,38 @@
 f = figure;
 pdf = pdf2d.pdf';
 
-c_lim = 0.1;
-width = 6;
-for c_id = 1:size(pdf, 2)
-    
-    if (c_id == 7)
-       ololo = 1; 
-    end
-    
-    start_row = 1;
-    for r_id = 1:101
-        if pdf(r_id, c_id) > c_lim
-            start_row = r_id;
-            break;
+is_crest = 0;
+
+if (is_crest == 0)
+
+    c_lim = 0.1;
+    width = 6;
+    for c_id = 1:size(pdf, 2)
+
+        if (c_id == 7)
+           ololo = 1; 
+        end
+
+        start_row = 1;
+        for r_id = 1:101
+            if pdf(r_id, c_id) > c_lim
+                start_row = r_id;
+                break;
+            end
+        end
+
+        if start_row < 101
+            mean_val = mean(pdf(start_row : 101 - width, c_id));
+            replace = ones(2*width + 1, 1) * mean_val + randn(2*width + 1, 1) * 0.01 * mean_val;
+            pdf(101 - width : 101 + width, c_id) = replace;
         end
     end
-    
-    if start_row < 101
-        mean_val = mean(pdf(start_row : 101 - width, c_id));
-        replace = ones(2*width + 1, 1) * mean_val + randn(2*width + 1, 1) * 0.01 * mean_val;
-        pdf(101 - width : 101 + width, c_id) = replace;
-    end
+
+    pdf(101, :) = 0.5 * (pdf(100, :) + pdf(102, :));
+
 end
 
-pdf(101, :) = 0.5 * (pdf(100, :) + pdf(102, :));
-
-pdf = smooth2a(pdf, 5, 5);
+%pdf = smooth2a(pdf, 5, 5);
 
 s = surf(pdf2d.x_bin_centers, pdf2d.y_bin_centers, pdf, 'FaceAlpha', 0.8);
 set(gca, 'FontSize', 30);
