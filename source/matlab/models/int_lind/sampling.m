d0a0d464107474e0b793f13cac30594a0fc0f2cd
Archie Hewitt <hewitt.archie@yandex.ru>
2021-10-08 01:51:18 +0300

    in progress

--- source/matlab/models/int_lind/sampling.m
+++ source/matlab/models/int_lind/sampling.m
@@ -53,111 +53,58 @@ for state_id = 1:num_states
     state_int = state_id - 1;
     state_bits = de2bi(state_int, N);
     
-    tmp = bi2de(state_bits);
-    
-    ololo = 1;
-end
-    
-
-imag_lim = 5e-3;
-
-%%% Step 1
-%%% NB: here we complete the F-basis by a normalized identity matrix as
-%%% F{1} - just for the future, but will not actually use it.
-k=1;
-F{1}=sparse(eye(N))/sqrt(N);
-for i=1:N
-    for j=i+1:N
-        k=k+1;
-        F{k}=sparse([i j],[j i],[1 1]/sqrt(2),N,N);
-        k=k+1;
-        F{k}=sparse([i j],[j i],-1i*[1 -1]/sqrt(2),N,N);
+    if (state_bits(1) == state_bits(end))
+        border_diss(state_id, state_id) = 1;
+    else
+        inv_bits = state_bits;
+        inv_bits(1) = state_bits(end);
+        inv_bits(end) = state_bits(1);
+        row_id = bi2de(inv_bits);
+        border_diss(row_id, state_id) = 1;
     end
 end
+dissipators{N} = border_diss;
 
-for i=1:N-1
-    k=k+1;
-    temp=zeros(1,i+1);
-    temp(1:i)=ones(1,i);
-    temp(i+1)=-i;
-    F{k}=sparse([1:i+1],[1:i+1],temp/sqrt(i*(i+1)),N,N);
+Lind = zeros(num_states^2);
+for diss_id = 1:size(dissipators, 1)
+    tmp = 0.5 * (2 * kron(eye(num_states), dissipators{diss_id}) * ...
+        kron(transpose(dissipators{diss_id}'), eye(num_states)) - ...
+        kron(transpose(dissipators{diss_id}' * dissipators{diss_id}), eye(num_states)) - ...
+        kron(eye(num_states), dissipators{diss_id}' * dissipators{diss_id}));
+    Lind = Lind + tmp;
 end
     
-all_evals = zeros(M * num_seeds, 1);
-zs_all = zeros(M * num_seeds, 1);
-s_id = 0;
-f_id = 0;
-for seed = 1:num_seeds
-    fprintf('seed = %d\n', seed);
-    tic
-    
-    %%% Step 2: calculate G-matrix
-    G = Ggen(seed, M, N);
-    
-    [Evec,D] = eig(G);
-    for k1=1:M
-        DISS{k1}=0;
-        for k2=1:M
-            DISS{k1}=DISS{k1} + Evec(k2,k1) * F{k2+1};
-        end
-    end
+%%% Step 5
+evals = eig(Lind);
+evals = sort(evals,'ComparisonMethod','abs');
+evals = evals(2:end);
+evals = (evals + 1) * N;
+all_evals((seed - 1) * M + 1 : seed * M) = evals;
 
-    %%% Step 3: calculate H-matrix
-    H = alpha * Hgen(seed, N) / sqrt(N);
+evals_filtered = evals(abs(imag(evals)) >= imag_lim);
+num_passed_evals = size(evals_filtered, 1);
+fprintf('num_passed_evals = %d\n', num_passed_evals);
 
-    %%% Step 4
-    P_origin = zeros(N^2); %% Initialize superopetator matrix
-    P_diss = zeros(N^2);
-    for k1=1:M
-        
-        P_diss=P_diss+D(k1,k1)/2*(2*kron(eye(N),DISS{k1})*kron(transpose(DISS{k1}'),eye(N))-kron(transpose(DISS{k1}'*DISS{k1}),eye(N))-kron(eye(N),DISS{k1}'*DISS{k1}));
-        
-        for k2=1:M
-            P_origin=P_origin+G(k1,k2)/2*(2*kron(eye(N),F{k1+1})*kron(transpose(F{k2+1}'),eye(N))-kron(transpose(F{k2+1}'*F{k1+1}),eye(N))-kron(eye(N),F{k2+1}'*F{k1+1}));
-        end
-    end
-    HamPart = -sqrt(-1)*(kron(eye(N),H)-kron(transpose(H),eye(N)));
-    P_origin = P_origin + HamPart;
-    P_diss = P_diss + HamPart;
-    
-    P_check = norm(P_origin - P_diss);
-    if (abs(P_check) > 1e-12)
-        fprintf("Lindbladians are differ!")
-    end
+s_id = f_id + 1;
+f_id = s_id + num_passed_evals - 1;
 
-    %%% Step 5
-    evals = eig(P_origin);
-    evals = sort(evals,'ComparisonMethod','abs');
-    evals = evals(2:end);
-    evals = (evals + 1) * N;
-    all_evals((seed - 1) * M + 1 : seed * M) = evals;
-    
-    evals_filtered = evals(abs(imag(evals)) >= imag_lim);
-    num_passed_evals = size(evals_filtered, 1);
-    fprintf('num_passed_evals = %d\n', num_passed_evals);
-    
-    s_id = f_id + 1;
-    f_id = s_id + num_passed_evals - 1;
-    
-    zs = zeros(num_passed_evals, 1);
-    neighbours_2D = horzcat(real(evals_filtered), imag(evals_filtered));
-    for z_id = 1 : num_passed_evals
-        target_2D = horzcat(real(evals_filtered(z_id)), imag(evals_filtered(z_id)));
-        target = evals_filtered(z_id);
-        distances = sqrt((neighbours_2D(:, 1) - target_2D(:, 1)).^2 + (neighbours_2D(:, 2) - target_2D(:, 2)).^2);
-        [min_distances, order] = mink(distances, 3);
-        nn = evals_filtered(order(2));
-        nnn = evals_filtered(order(3));
-        zs(z_id) = (nn - target) / (nnn - target);
-    end
-    
-    num_zero_zs = sum(abs(zs) < 1e-4);
-    fprintf('num_zero_zs = %d\n', num_zero_zs);
-    
-    zs_all(s_id : f_id) = zs;
-    
-    toc
+zs = zeros(num_passed_evals, 1);
+neighbours_2D = horzcat(real(evals_filtered), imag(evals_filtered));
+for z_id = 1 : num_passed_evals
+    target_2D = horzcat(real(evals_filtered(z_id)), imag(evals_filtered(z_id)));
+    target = evals_filtered(z_id);
+    distances = sqrt((neighbours_2D(:, 1) - target_2D(:, 1)).^2 + (neighbours_2D(:, 2) - target_2D(:, 2)).^2);
+    [min_distances, order] = mink(distances, 3);
+    nn = evals_filtered(order(2));
+    nnn = evals_filtered(order(3));
+    zs(z_id) = (nn - target) / (nnn - target);
 end
+
+num_zero_zs = sum(abs(zs) < 1e-4);
+fprintf('num_zero_zs = %d\n', num_zero_zs);
+
+zs_all(s_id : f_id) = zs;
+
 
 zs_all = zs_all(1 : f_id);
 
